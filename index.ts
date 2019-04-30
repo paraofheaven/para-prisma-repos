bcae1746cdfc76e1879600e6f8323263211ea5d7
paraofheaven <1549930735@qq.com>
2019-04-30 10:56:47 +0800

    update

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ async function main() {
   // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
   // Read all users from the database and print them to the console
-  
+
 
   // const user = await prisma.user({ id: 'cjv25snto001207038baa6o32' });
   // console.log(user);
@@ -24,10 +24,32 @@ async function main() {
   //   data: { name: 'Bob' },
   // })
 
-  await prisma.deleteUser({ id: 'cjv25snto001207038baa6o32' })
+  // await prisma.deleteUser({ id: 'cjv25snto001207038baa6o32' })
 
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+  // const allUsers = await prisma.users()
+  // console.log(allUsers)
+
+  // const newUser = await prisma.createUser({
+  //   name: 'Chalice',
+  //   email: 'Chalice@prisma.io',
+  //   posts: {
+  //     create: [
+  //       {
+  //         title: 'Join us for GraphQL Conf in 2019',
+  //       },
+  //       {
+  //         title: 'Subscribe to GraphQL Weekly for GraphQL news',
+  //       }
+  //     ]
+  //   }
+  // });
+  // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+
+  // const allUsers = await prisma.users();
+  // console.log(allUsers);
+
+  // const allPosts = await prisma.posts();
+  // console.log(allPosts);
 }
 
 main().catch(e => console.error(e))
